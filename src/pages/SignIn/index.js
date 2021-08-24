2e5cefaf731940bc48b637e55e574717bb3a5ff4
jorgelucasalima <jorgelucasalima@gmail.com>
2021-08-24 14:22:50 -0300

    alterção no css de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,32 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './signin.css'
+import logo from '../../assets/logo.png'
+
 
 function SignIn() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPasswird] = useState('')
+
+
     return (
-      <div>
-        <h1>PAGINA DE LOGIN</h1>
+      <div className="container-center">
+        <div className="login">
+          <div className="login-area">
+            <img src={logo} alt="Logo" />
+          </div>
+
+          <form>
+            <h1>Entrar</h1>
+            <input type="text" placeholder="email@email.com" />
+            <input type="password" placeholder="****" />
+            <button type="submit">Acessar</button>
+          </form>
+
+          <Link to="/register">Criar uma conta</Link>
+
+        </div>
       </div>
     );
   }
