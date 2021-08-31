259f958f4b2c00022e7d672a3dc3cafbdd1ec55d
jorgelucasalima <jorgelucasalima@gmail.com>
2021-08-31 10:19:45 -0300

    criado states/function do customers

--- src/pages/Customers/index.js
+++ src/pages/Customers/index.js
@@ -1,9 +1,51 @@
+import { useState } from 'react'
+
 import './customers.css'
+import Title from '../../components/Title'
+import Header from '../../components/Header'
+import { FiUser } from 'react-icons/fi'
+
 
 export default function Customers(params) {
-  return(
+  
+
+  //STATES
+  const [nomeFantasia, setNomeFantasia] = useState('');
+  const [cnpj, setCnpj] = useState('');
+  const [endereco, setEndereco] = useState('');
+  
+
+  //FUNÇÕES
+  function handleAdd(e) {
+    e.preventDefault()
+    alert('teste')
+  }
+  
+  return(  
     <div>
-      <h1>Pagina clientes</h1>
+      <Header/>
+
+      <div className="content">
+        <Title name="Clientes">
+          <FiUser size={25} />
+        </Title>
+
+        <div className="container">
+          <form className="form-profile customers" onSubmit={handleAdd}>
+            <label>Nome Empresa</label>
+            <input type="text" placeholder="Nome da sua Empresa" value={nomeFantasia} onChange={ (e) => setNomeFantasia(e.target.value)} />
+            
+            <label>CNPJ</label>
+            <input type="text" placeholder="Seu Cnpj" value={cnpj} onChange={ (e) => setCnpj(e.target.value) } />
+            
+            <label>Endereço</label>
+            <input type="text" placeholder="Endereço da Empresa" value={endereco} onChange={ (e) => setEndereco(e.target.value)} />
+            
+            <button type="submit">Cadastrar</button>
+          </form>
+        </div>
+      </div>
+    
     </div>
   )
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
 import Dashboard from '../pages/Dashboard'
 import Profile from '../pages/Profile'
+import Customers from '../pages/Customers'
 
 
 
@@ -15,6 +16,7 @@ export default function Routes(){
             <Route exact path="/" component={SignIn} />
             <Route exact path="/register" component={SignUp} />
             <Route exact path="/dashboard" component={Dashboard} isPrivate />
+            <Route exact path="/customers" component={Customers} isPrivate />
             <Route exact path="/profile" component={Profile} isPrivate />
         </Switch>
     )
