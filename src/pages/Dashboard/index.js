04d06e1a85dcc9695cc84d7afbadc8e46566ac44
jorgelucasalima <jorgelucasalima@gmail.com>
2021-08-31 11:09:23 -0300

    criado pages de chamados/tickets

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,20 +1,51 @@
-import { useContext } from "react"
-import { AuthContext } from '../../contexts/auth'
+import { useState } from "react"
+
 import Header from "../../components/Header"
+import Title from '../../components/Title'
+import './dashboard.css'
+import { FiMessageSquare, FiPlus } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
 
 
 
 export default function Dashboard(){
 
-    const { signOut } = useContext(AuthContext)
+    const [chamados, setChamados] = useState([]);
 
     return(
         <div>
             <Header/>
 
+            <div className="content">
+                <Title name="Tickets">
+                    <FiMessageSquare size={25} />
+                </Title>  
 
-            <h1>DASHBOARD</h1>
-        
+                {chamados.length === 0 ? (
+
+                    <div className="container dashboard">
+                    <span>Nenhum Ticket aberto...</span>
+
+                    <Link to="/new" className="new">
+                        <FiPlus size={25} color="#FFF" />
+                        Novo Chamado
+                    </Link>
+                    </div>
+
+                ) : (
+                    <>
+                        <Link to="/new" className="new">
+                            <FiPlus size={25} color="#FFF" />
+                            Novo Chamado
+                        </Link>
+                    </>
+                )}
+
+       
+
+            </div>
+
         </div>
     )
 }
