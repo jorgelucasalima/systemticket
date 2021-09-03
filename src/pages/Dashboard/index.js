2ecb2df1fa3e70b69c1e08c14158f10423e02038
jorgelucasalima <jorgelucasalima@gmail.com>
2021-09-03 16:04:48 -0300

    buscando dados do firebase

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,91 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import firebase from '../../services/firebaseConnections'
 
 import Header from "../../components/Header"
 import Title from '../../components/Title'
 import './dashboard.css'
 import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { format } from 'date-fns'
 
 
+
+const listRef = firebase.firestore().collection('tickets').orderBy('created', 'desc')
 
 
 export default function Dashboard(){
 
-    const [chamados, setChamados] = useState([1]);
+    const [chamados, setChamados] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [loadingMore, setLoadingMore] = useState(false)
+    const [isEmpty, setIsEmpty] = useState(false);
+    const [lastDocs, setLastDocs] = useState()
+
+
+    useEffect(() => {
+        
+        loadChamados()
+
+
+        return () => {
+            
+        };
+
+    }, []);
+
+
+
+    //FUNÇÕES
+    async function loadChamados() {
+        await listRef.limit(5)
+        .get()
+        .then((snapshort) => {
+            updateState(snapshort)
+        })
+        .catch((error) => {
+            console.log('ocorreu algum erro no loadchamado', error)
+            setLoadingMore(false)
+        })
+        setLoading(false)
+    }
+
+
+    async function updateState(snapshort) {
+        const isCollectionEmpty = snapshort.size === 0
+
+        if (!isCollectionEmpty) {
+            let lista = []
+
+            snapshort.forEach((doc) => {
+                lista.push({
+                    id: doc.id,
+                    assunto: doc.data().assunto,
+                    cliente: doc.data().cliente,
+                    clienteId: doc.data().clienteId,
+                    created: doc.data().created,
+                    createdFormated: format(doc.data().created.toDate(),'dd/MM/yyyy'),
+                    status: doc.data().status,
+                    complemento: doc.data().complemento
+
+                })
+            })
+
+            const lastDoc = snapshort.docs[snapshort.docs.length -1] //pegando ultimo documento buscado
+
+            setChamados(chamados => [...chamados, ...lista])
+            setLastDocs(lastDoc)
+
+        }else{
+            setIsEmpty(true)
+        }
+
+        setLoadingMore(false)
+            
+    }
+
+
+
+
 
     return(
         <div>
